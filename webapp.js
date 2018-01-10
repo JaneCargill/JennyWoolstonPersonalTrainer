72bf7dfcc21368a013c161f17e10ff82a22fd122
JaneCargill <jane-cargill@hotmail.co.uk>
2018-01-10 18:28:49 +0000

    node express

---
+++ webapp.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var url = require('url');
+var show = require('./show');
+
+
+http.createServer(onRequest).listen(3000);
+console.log('Server has started');
+
+/*function onRequest(request, response){
+  response.writeHead(200);
+  response.write('Hello Noders');
+  response.end();
+}*/
+
+function onRequest(request, response){
+  var pathName = url.parse(request.url).pathname;
+  console.log('pathname' + pathName);
+  show.showPage(response, pathName)
+}
+
+
+
