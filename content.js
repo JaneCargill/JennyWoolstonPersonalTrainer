e4dba4a460992a5f527eccbf3256446207932ae2
JaneCargill <jane-cargill@hotmail.co.uk>
2018-01-11 23:24:56 +0000

    html added to home page

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
+var html = require('public/index.html');
 var contentMap = {
 
- '/': '<h1>Welcome to the site</h1>',
+ '/': 'html',
  '/contact': '<h1>Contact Page</h1>',
  '/about': '<h1>About Page</h1>'
 }
