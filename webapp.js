e9fb427ebc5a5bc731dfb59422f1501da4762235
JaneCargill <jane-cargill@hotmail.co.uk>
2018-01-12 21:43:50 +0000

    CAPS FOR PORT

--- webapp.js
+++ webapp.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = process.env.port || 5000;
+var port = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
