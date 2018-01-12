48760a7b7bc933ac4373d96fb72bae9fb8dc978d
JaneCargill <jane-cargill@hotmail.co.uk>
2018-01-12 21:15:31 +0000

    tidy up files removed

--- content.js
+++
@@ -1,9 +0,0 @@
-
-var contentMap = {
-
- '/': '<h1>Welcome</h1>',
- '/contact': '<h1>Contact Page</h1>',
- '/about': '<h1>About Page</h1>'
-}
-
-exports.contentMap = contentMap;
--- webapp.js
+++ webapp.js
@@ -1,27 +1,3 @@
-/*var http = require('http');
-var url = require('url');
-var show = require('./show');
-
-
-http.createServer(onRequest).listen(process.env.PORT || 5000);
-console.log('Server has started');
-
-/*function onRequest(request, response){
-  response.writeHead(200);
-  response.write('Hello Noders');
-  response.end();
-}*/
-
-/*function doGet() {
-  return HtmlService.createHtmlOutputFromFile('/public/index.html');
-}
-
-function onRequest(request, response){
-  var pathName = url.parse(request.url).pathname;
-  console.log('pathname' + pathName);
-  show.showPage(response, pathName)
-}*/
-
 
 var express = require('express');
 var app = express();
